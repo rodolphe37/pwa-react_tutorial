96d3cc00ea63861db84c95f0eca5ef77091ad268
rodolphe37 <rodolphe.a@gmail.com>
2020-08-15 10:35:57 +0200

    deleted default src folder and doing application bases (recreate src folder, install axios and create index, app's and fetch api files for weather progressive app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import fetchWeather from './api/fetchWeather';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [query, setQuery] = useState('')
+
+  const search = async (e) => {
+    if (e.key === 'Enter') {
+      const data = await fetchWeather(query)
+
+      console.log(data)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+      <input
+        type="text"
+        className="search"
+        placeholder="Search ..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        onKeyPress={search}
+      />
     </div>
-  );
+  )
+
 }
 
 export default App;
