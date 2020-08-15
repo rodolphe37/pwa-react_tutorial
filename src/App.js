eb328f05b7a364299c3bceb9ec587f4ded81f34f
rodolphe37 <rodolphe.a@gmail.com>
2020-08-15 11:19:31 +0200

    API consumption and code creation for informations visibility

--- src/App.js
+++ src/App.js
@@ -4,25 +4,51 @@ import './App.css';
 
 const App = () => {
   const [query, setQuery] = useState('')
+  const [weather, setWeather] = useState({})
 
   const search = async (e) => {
     if (e.key === 'Enter') {
       const data = await fetchWeather(query)
-
       console.log(data)
+      setWeather(data)
+      setQuery('')
     }
   }
 
   return (
     <div className="main-container">
-      <input
-        type="text"
-        className="search"
-        placeholder="Search ..."
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-        onKeyPress={search}
-      />
+      <input type="text" className="search" placeholder="Search ..." value={query} onChange={(e) => setQuery(e.target.value)} onKeyPress={search} />
+      {weather.main && (
+        <div className="city">
+          <h2 className="city-name">
+            <span>{weather.name}</span>
+            <sup>{weather.sys.country}</sup>
+          </h2>
+          <div className="city-temp">
+            {Math.round(weather.main.temp)}
+            <sup>&deg;C</sup>
+          </div>
+          <div className="city-feels-humidity">
+            <p>Humidité</p>
+            {weather.main.humidity}
+            <sup>%</sup>
+          </div>
+          <div className="weather-desc">
+            <span>{weather.weather[0].description}</span>
+          </div>
+          <div className="weather-deg-wind">
+            <span>{weather.wind.deg}</span>
+            <sup>&deg;</sup>
+          </div>
+          <div className="weather-speed-wind">
+            <span>{weather.wind.speed}</span>
+            <sup> Km/h</sup>
+          </div>
+          <div className="info">
+            <img className="city-icon" src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
+          </div>
+        </div>
+      )}
     </div>
   )
 
